de7e10666b5b039caffb7bfb279f397168ed1c96
William Koller <wkoller25@gmail.com>
2020-02-06 14:56:44 -0300

    adding methods HTTP

--- src/app/infra/lista-dao.js
+++ src/app/infra/lista-dao.js
@@ -15,6 +15,7 @@ class LivroDao {
                     livro.preco,
                     livro.descricao
                 ],
+                // callback 
                 function(err) {
                     if (err) {
                         console.log(err);
@@ -36,6 +37,62 @@ class LivroDao {
                     return resolve(resultados);
                 }
             )
+        });
+    }
+    buscaPorId(id) {
+        return new Promise((resolve, reject) => {
+            this._db.get(
+                `
+                    SELECT * FROM livros WHERE id = ?
+                    
+                `, [id],
+                (erro, livro) => {
+                    if (erro) {
+                        return reject('Não foi possível encontrar o livro');
+                    }
+                    return resolve(livro);
+                }
+            )
+        });
+    }
+
+    atualiza(livro) {
+        return new Promise((resolve, reject) => {
+            this._db.run(`
+            UPDATE livros SET 
+            titulo = ?,
+            preco = ?,
+            descricao = ?,
+            WHERE id =?
+            `, [
+                    livro.titulo,
+                    livro.preco,
+                    livro.descricao,
+                    livro.id
+                ],
+                (erro) => {
+                    if (erro) {
+                        return reject('Não foi possível alualizar o livro');
+                    }
+                    resolve();
+                });
+        });
+    }
+
+    remove(id) {
+        return new Promise((resolve, reject) => {
+            this._db.run(
+                `
+                DELETE 
+                FROM livros WHERE id = ?
+                `, [id],
+                (erro) => {
+                    if (erro) {
+                        return reject('Não foi possível deletar esse livro');
+                    }
+                    return resolve();
+                }
+            );
         });
     }
 }
