97679f150a64c7912790c702c5be343bdbe144da
William Koller <wkoller25@gmail.com>
2020-02-07 10:12:55 -0300

    defined routes

--- src/app/rotas/rotas.js
+++ src/app/rotas/rotas.js
@@ -1,58 +1,62 @@
 const LivroDao = require('../infra/lista-dao');
 const db = require('../../config/database');
+
 module.exports = (app) => {
     app.get('/', function(req, resp) {
         resp.send(
-            `<!DOCTYPE html>
-  <html lang="en">
-  <head>
-    <meta charset="UTF-8"/>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-    <meta http-equiv="X-UA-Compatible" content="ie=edge"/>
-    <title>Nodejs with express</title>
-  </head>
-  <body>
-    <h1>Nodejs with express</h1>
-    <buttom><a href="/livros">Livros</a></buttom>
-  </body>
-  </html>`
+            `
+                <html>
+                    <head>
+                        <meta charset="utf-8">
+                    </head>
+                    <body>
+                        <h1> Casa do Código </h1>
+                    </body>
+                </html>
+            `
         );
     });
 
     app.get('/livros', function(req, resp) {
+
         const livroDao = new LivroDao(db);
-        livroDao.lista().then(livros => resp.marko(
-            require('../views/livros/lista/lista.marko'), {
-                livros: livros
-            }
-        )).catch(erro => console.log(erro));
+        livroDao.lista()
+            .then(livros => resp.marko(
+                require('../views/livros/lista/lista.marko'), {
+                    livros: livros
+                }
+            ))
+            .catch(erro => console.log(erro));
     });
 
-    // app é o objeto do express - function() é o callback
     app.get('/livros/form', function(req, resp) {
-        resp.marko(
-            require('../views/livros/form/form.marko'), { livro: {} });
+        resp.marko(require('../views/livros/form/form.marko'), { livro: {} });
     });
 
     app.get('/livros/form/:id', function(req, resp) {
-        const { id } = req.params.id;
+        const id = req.params.id;
         const livroDao = new LivroDao(db);
 
         livroDao.buscaPorId(id)
-            .then(livro => resp.marko(
-                require('../views/livros/form/form.marko'), { livro })).catch(erro => console.log(erro));
+            .then(livro =>
+                resp.marko(
+                    require('../views/livros/form/form.marko'), { livro: livro }
+                )
+            )
+            .catch(erro => console.log(erro));
     });
 
     app.post('/livros', function(req, resp) {
         console.log(req.body);
         const livroDao = new LivroDao(db);
+
         livroDao.adiciona(req.body)
             .then(resp.redirect('/livros'))
             .catch(erro => console.log(erro));
     });
 
     app.put('/livros', function(req, resp) {
-
+        console.log(req.body);
         const livroDao = new LivroDao(db);
 
         livroDao.atualiza(req.body)
@@ -62,6 +66,7 @@ module.exports = (app) => {
 
     app.delete('/livros/:id', function(req, resp) {
         const id = req.params.id;
+
         const livroDao = new LivroDao(db);
         livroDao.remove(id)
             .then(() => resp.status(200).end())
