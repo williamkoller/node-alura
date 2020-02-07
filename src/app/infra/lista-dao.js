d52d58ba62f3a4c3f1c7c8c0195b364a057a2430
William Koller <wkoller25@gmail.com>
2020-02-07 10:12:40 -0300

    adding crud sqlite

--- src/app/infra/lista-dao.js
+++ src/app/infra/lista-dao.js
@@ -1,69 +1,74 @@
 class LivroDao {
+
     constructor(db) {
         this._db = db;
     }
+
     adiciona(livro) {
         return new Promise((resolve, reject) => {
             this._db.run(`
                 INSERT INTO livros (
-                    titulo,
+                    titulo, 
                     preco,
                     descricao
-                ) VALUES (?,?,?)
+                ) values (?,?,?)
                 `, [
                     livro.titulo,
                     livro.preco,
                     livro.descricao
                 ],
-                // callback 
                 function(err) {
                     if (err) {
                         console.log(err);
-                        return reject('Não foi possível adicionar o livro');
+                        return reject('Não foi possível adicionar o livro!');
                     }
+
                     resolve();
                 }
             )
         });
     }
+
     lista() {
         return new Promise((resolve, reject) => {
             this._db.all(
                 'SELECT * FROM livros',
                 (erro, resultados) => {
-                    if (erro) return reject(
-                        `Não foi possivel listar os livros`
-                    );
+                    if (erro) return reject('Não foi possível listar os livros!');
+
                     return resolve(resultados);
                 }
             )
         });
     }
+
     buscaPorId(id) {
+
         return new Promise((resolve, reject) => {
             this._db.get(
                 `
-                    SELECT * FROM livros WHERE id = ?
-                    
+                    SELECT *
+                    FROM livros
+                    WHERE id = ?
                 `, [id],
                 (erro, livro) => {
                     if (erro) {
-                        return reject('Não foi possível encontrar o livro');
+                        return reject('Não foi possível encontrar o livro!');
                     }
                     return resolve(livro);
                 }
-            )
+            );
         });
     }
 
     atualiza(livro) {
         return new Promise((resolve, reject) => {
             this._db.run(`
-            UPDATE livros SET 
-            titulo = ?,
-            preco = ?,
-            descricao = ?,
-            WHERE id = ?
+                UPDATE livros SET
+                titulo = ?,
+                preco = ?,
+                descricao = ?
+                WHERE id = ?
             `, [
                     livro.titulo,
                     livro.preco,
@@ -72,23 +77,26 @@ class LivroDao {
                 ],
                 erro => {
                     if (erro) {
-                        return reject('Não foi possível atualizar o livro');
+                        return reject('Não foi possível atualizar o livro!');
                     }
+
                     resolve();
                 });
         });
     }
 
     remove(id) {
+
         return new Promise((resolve, reject) => {
-            this._db.run(
+            this._db.get(
                 `
-                DELETE 
-                FROM livros WHERE id = ?
+                    DELETE 
+                    FROM livros
+                    WHERE id = ?
                 `, [id],
                 (erro) => {
                     if (erro) {
-                        return reject('Não foi possível deletar esse livro');
+                        return reject('Não foi possível remover o livro!');
                     }
                     return resolve();
                 }
@@ -96,4 +104,5 @@ class LivroDao {
         });
     }
 }
+
 module.exports = LivroDao;
