f17a5f3e78e6f8a08eee5116d63914cffe172cf9
William Koller <wkoller25@gmail.com>
2020-02-07 10:13:57 -0300

    defined custom-express

--- src/config/custom-express.js
+++ src/config/custom-express.js
@@ -1,17 +1,16 @@
 require('marko/node-require').install();
 require('marko/express');
 
-// import express
 const express = require('express');
-// init express
 const app = express();
-// import bodyParser middlaware
 const bodyParser = require('body-parser');
 const methodOverride = require('method-override');
 
-
 app.use('/estatico', express.static('src/app/public'));
 
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 app.use(methodOverride(function(req, res) {
     if (req.body && typeof req.body === 'object' && '_method' in req.body) {
         // look in urlencoded POST bodies and delete it
@@ -20,13 +19,8 @@ app.use(methodOverride(function(req, res) {
         return method;
     }
 }));
-app.use(bodyParser.urlencoded({
-    extended: true
-}));
 
-// import rotas
 const rotas = require('../app/rotas/rotas');
-// init rotas w/ params app
 rotas(app);
-// export app
+
 module.exports = app;
