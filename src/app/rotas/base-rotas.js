c44e5b93c0b1684f8abf23a2c8b6fd96c2097d92
William Koller <wkoller25@gmail.com>
2020-02-18 23:10:07 -0300

    correction routes

--- src/app/rotas/base-rotas.js
+++ src/app/rotas/base-rotas.js
@@ -1,10 +1,11 @@
+const express = require("express");
+const router = express.Router();
 const BaseController = require("../controllers/baseController");
 const baseController = new BaseController();
-module.exports = app => {
+module.exports = router => {
   const rotasBase = BaseController.rotas();
-  app.get(rotasBase.home, baseController.home());
-  app
-    .router(rotasBase.login)
-    .get(baseController.login())
-    .post(baseController.efetuarLogin());
+  router.get(rotasBase.home, baseController.home());
+  router.get(rotasBase.login);
+  router.get(baseController.login());
+  router.post(baseController.efetuarLogin());
 };
