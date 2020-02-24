1ef9839c548498f2ccff1e80297fbdd7119f9d1d
William Koller <wkoller25@gmail.com>
2020-02-24 14:41:00 -0300

    finish autorization

--- src/app/rotas/livro-rotas.js
+++ src/app/rotas/livro-rotas.js
@@ -1,9 +1,18 @@
 const LivroController = require("../controllers/livroController");
 const livroController = new LivroController();
 const Livro = require("../modelos/livro");
+const BaseController = require("../controllers/baseController");
 
 module.exports = app => {
   const rotasLivro = LivroController.rotas();
+
+  app.use(rotasLivro.autenticadas, function(req, resp, next) {
+    if (req.isAuthenticated()) {
+      next();
+    } else {
+      resp.redirect(BaseController.rotas().login);
+    }
+  });
   app.get(rotasLivro.lista, livroController.lista());
 
   app
